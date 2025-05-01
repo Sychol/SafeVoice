9410a2e5e1cd1ea64836421ba595cd42d0ec0bca
smhrd <smhrd@192.168.219.86>
2025-05-01 15:55:14 +0900

    2025.05.01 ALERT DAO 작업 완료

--- SafeVoice/src/main/java/com/safevoice/db/AlertDAO.java
+++ SafeVoice/src/main/java/com/safevoice/db/AlertDAO.java
@@ -1,17 +1,31 @@
 package com.safevoice.db;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.safevoice.model.AlertVO;
+import com.safevoice.model.MemberVO;
 
 public class AlertDAO {
 
 	private SqlSessionFactory factory = MySqlSessionManager.getFactory();
 	
-	public void GetAlertHistory() {
-		
+	// 조회하려는 아이디에 따른 모든 Alert 를 확인하는 메서드 
+	public List<AlertVO> getAlertHistory(MemberVO member) {
+		SqlSession sqlsession = factory.openSession(true);
+		List<AlertVO> alertList = sqlsession.selectList("getAlertHistory", member);
+		sqlsession.close();
+		return alertList;
 	}
 	
-	public void SetAlertInfo() {
-		
+	// 분석 결과를 DB에 입력하려는 메서드
+	public int setAlertInfo(AlertVO alert) {
+		SqlSession sqlsession = factory.openSession(true);
+		int row = sqlsession.insert("setAlertInfo", alert);
+		sqlsession.close();
+		return row;
 	}
 	
 	

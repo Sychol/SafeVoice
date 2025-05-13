b50ab1f2dc05f614d49bdda59690a13a31040ea9
smhrd <smhrd@192.168.219.86>
2025-05-13 16:37:20 +0900

    2025.05.13 16:40 commit

--- SafeVoice/src/main/java/com/safevoice/controller/Alert/GetAlertHistoryService.java
+++ SafeVoice/src/main/java/com/safevoice/controller/Alert/GetAlertHistoryService.java
@@ -11,6 +11,7 @@ import com.google.gson.Gson;
 import com.mysql.cj.Session;
 import com.safevoice.controller.Command;
 import com.safevoice.db.AlertDAO;
+import com.safevoice.db.MemberDAO;
 import com.safevoice.model.AlertVO;
 import com.safevoice.model.MemberVO;
 
@@ -19,7 +20,7 @@ public class GetAlertHistoryService implements Command {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// 아이디 전역 변수 처리
 		String id = "";
-		// 조회하려는 아이디를 session 에서 가져오기. 
+		// 조회하려는 자녀 아이디를 session 에서 가져오기. 
 		HttpSession session = request.getSession();
 		List<MemberVO> childList = (List<MemberVO>) session.getAttribute("childList");
 		if(childList != null && !childList.isEmpty()) {
@@ -34,6 +35,10 @@ public class GetAlertHistoryService implements Command {
 		
 		// adao의 getAlertHistory 메서드를 실행시키고 결과값을 list에 저장
 		List<AlertVO> alertList = adao.getAlertHistory(id);
+		
+		// MemberDAO 생성
+		MemberDAO dao = new MemberDAO();
+		
 		
 		// List 형태로 온 데이터를 json 형식으로 변경
 		Gson gson = new Gson();
